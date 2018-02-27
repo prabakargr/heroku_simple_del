8985e26ca0b98cbaf402fd3e4762cf9bec83ff46
Jagan Raja <jaganraja479@gmail.com>
2018-02-27 15:08:39 +0530

    add user

--- users/usersRouting.js
+++ users/usersRouting.js
@@ -5,6 +5,7 @@ var usersController=require('./userscontroller');
 var usersRouting=express.Router();
 
 usersRouting.route('/getusers').get(usersController.getusers);
+usersRouting.route('/adduser').post(usersController.adduser);
 
 
 module.exports=usersRouting;
--- users/userscontroller.js
+++ users/userscontroller.js
@@ -1,4 +1,18 @@
 var User=require('./userModel');
+
+//add user
+var adduser=function (req,res) {
+    var user=new User(req.body);
+    user.save(function (err) {
+        if(err){
+            res.send('cannot reqister')
+        }else{
+            res.send(user)
+        }
+    });
+};
+
+//get users
 var getusers=function(req,res){
     User.find(function(err,users){
         console.log(users)
@@ -6,4 +20,7 @@ var getusers=function(req,res){
     })
 }
 
-module.exports.getusers=getusers;
+module.exports={
+    getusers:getusers,
+    adduser:adduser,
+}
