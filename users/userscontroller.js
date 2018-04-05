21ff3455bf64c5b1155a5e91f0b19b78cf7c0524
prabakargr <prabakar1993@gmail.com>
2018-04-05 14:27:17 +0530

    login changes

--- users/userscontroller.js
+++ users/userscontroller.js
@@ -187,7 +187,7 @@ var changepwd = function(req, res){
         };
         console.log(payload);
             var token = jwt.sign(payload, app.get('superSecret'),{
-             expiresInMinutes: 1440 // expires in 24 hours
+            // expiresIn: 1440 // expires in 24 hours
            
             });
             console.log(token);
